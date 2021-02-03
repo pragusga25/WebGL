4761d03af21d6ded4fce6eeb99cc10888ee6c15b
pragusga <pragusga25@gmail.com>
2021-02-03 14:00:28 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import _ from 'lodash';
+
+// const component = () => {
+//   const element = document.createElement('div');
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   return element;
+// };
+
+// document.body.appendChild(component());
+
+console.log('Hello Wordl');
