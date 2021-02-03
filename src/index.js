f009ab79f37a02de40ae5fa570d5ce01efba2a8f
pragusga <pragusga25@gmail.com>
2021-02-03 20:43:28 +0700

    create home page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,63 @@
-// import _ from 'lodash';
+import * as THREE from 'three';
 
-// const component = () => {
-//   const element = document.createElement('div');
+const home = document.getElementById('home');
 
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const menus = document.querySelectorAll('.menu');
 
-//   return element;
-// };
+// menus.addEventListener('mouseover', () => {
+//   menus.style.display = 'none';
+// });
 
-// document.body.appendChild(component());
+menus.forEach((menu) => {
+  const child = menu.childNodes[0];
+  menu.addEventListener('mouseenter', () => {
+    const text = menu.getAttribute('nama');
+    menu.innerHTML = `<span class="appear">${text}</span>`;
+  });
+  menu.addEventListener('mouseleave', () => {
+    const textChild = menu.childNodes[0];
+    textChild.remove();
+    menu.appendChild(child);
+  });
+});
 
-console.log('Hello Wordl');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor('#121212');
+renderer.setSize(window.innerWidth, window.innerHeight);
+home.appendChild(renderer.domElement);
+
+const cubegeo = new THREE.BoxGeometry(2, 2, 2);
+const myTexture = new THREE.TextureLoader().load('../texture/texturea.jpg');
+const material = new THREE.MeshBasicMaterial({
+  // color: 0xfe4a49,
+  // wireframe: false,
+  map: myTexture,
+});
+const cube = new THREE.Mesh(cubegeo, material);
+cube.position.set(-2, 1, 0);
+scene.add(cube);
+
+camera.position.z = 5;
+
+window.addEventListener('resize', function () {
+  renderer.setSize(this.window.innerWidth, this.window.innerHeight);
+  camera.aspect = this.window.innerWidth / this.window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  cube.rotateY(0.02);
+  renderer.render(scene, camera);
+};
+
+animate();
