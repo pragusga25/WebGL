ff0d01f913ca59400c3729476bd3244a5694f842
pragusga <pragusga25@gmail.com>
2021-02-04 12:14:23 +0700

    Create typing animation

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+const menus = document.querySelectorAll('.menu');
+menus.forEach((menu) => {
+  const child = menu.childNodes[0];
+  menu.addEventListener('mouseover', () => {
+    console.log('enter');
+    const text = menu.getAttribute('nama');
+    menu.innerHTML = `${text}`;
+  });
+  menu.addEventListener('mouseleave', () => {
+    console.log('leave');
+    const textChild = menu.childNodes[0];
+    textChild.remove();
+    menu.appendChild(child);
+  });
+});
--- src/typing.js
+++ src/typing.js
@@ -1,66 +1,66 @@
-// // Type Animation
+// Type Animation
 
-// const TypeWriter = function (txtElement, words, wait = 3000) {
-//   this.txtElement = txtElement;
-//   this.words = words;
-//   this.txt = '';
-//   this.wordIndex = 0;
-//   this.wait = parseInt(wait, 10);
-//   this.type();
-//   this.isDeleting = false;
-// };
+const TypeWriter = function (txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
 
-// // Type Method
-// TypeWriter.prototype.type = function () {
-//   // current index of word
-//   const current = this.wordIndex % this.words.length;
-//   // get full text of current word
-//   const fullTxt = this.words[current];
+// Type Method
+TypeWriter.prototype.type = function () {
+  // current index of word
+  const current = this.wordIndex % this.words.length;
+  // get full text of current word
+  const fullTxt = this.words[current];
 
-//   // check if deleting
-//   if (this.isDeleting) {
-//     // remove char
-//     this.txt = fullTxt.substring(0, this.txt.length - 1);
-//   } else {
-//     // add char
-//     this.txt = fullTxt.substring(0, this.txt.length + 1);
-//   }
+  // check if deleting
+  if (this.isDeleting) {
+    // remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-//   // insert txt into element
+  // insert txt into element
+  const wordNow = this.wordIndex % 4;
+  this.txtElement.innerHTML = `<span class="txt txt-${wordNow}" data-text="${this.txt}">${this.txt}</span>`;
 
-//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  // Initial type speed
+  let typeSpeed = 200;
 
-//   // Initial type speed
-//   let typeSpeed = 200;
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
 
-//   if (this.isDeleting) {
-//     typeSpeed /= 2;
-//   }
+  // If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // make pause at end
+    typeSpeed = this.wait;
+    // set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    // move to next word
+    this.wordIndex++;
+    // pause before start typing
+    typeSpeed = 500;
+  }
 
-//   // If word is complete
-//   if (!this.isDeleting && this.txt === fullTxt) {
-//     // make pause at end
-//     typeSpeed = this.wait;
-//     // set delete to true
-//     this.isDeleting = true;
-//   } else if (this.isDeleting && this.txt === '') {
-//     this.isDeleting = false;
-//     // move to next word
-//     this.wordIndex++;
-//     // pause before start typing
-//     typeSpeed = 500;
-//   }
+  setTimeout(() => this.type(), typeSpeed);
+};
 
-//   setTimeout(() => this.type(), typeSpeed);
-// };
+// init on DOM Load
+document.addEventListener('DOMContentLoaded', init);
 
-// // init on DOM Load
-// document.addEventListener('DOMContentLoaded', init);
-
-// function init() {
-//   const txtElement = document.querySelector('.txt-type');
-//   const words = JSON.parse(txtElement.getAttribute('data-words'));
-//   const wait = txtElement.getAttribute('data-wait');
-//   // init TypeWriter
-//   new TypeWriter(txtElement, words, wait);
-// }
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
